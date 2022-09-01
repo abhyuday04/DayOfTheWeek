9c3cfb4fda282f4be9a000dcc2f9cc1cd60d89a4
abhyuday04 <abhyuday04@gmail.com>
2022-09-01 14:53:27 +0530

    Add project files.

---
+++ CalendarClass/CalenderClass.cs
@@ -0,0 +1,40 @@
+﻿namespace CalenderLibrary1
+
+{
+     public class CalenderClass
+    {
+        public static void Main()
+        {
+            CalenderClass cc = new();
+            Console.WriteLine(" Input the Date: ");
+            DateTime inputDate = DateTime.Parse(Console.ReadLine());
+            int day = inputDate.Day;
+            int m = inputDate.Month;
+            int y = inputDate.Year;
+            int d = CalenderFunction.dayOfDate(day, m, y);
+            Console.WriteLine("The day on this date is: " + Day(d));
+            Console.ReadLine();
+
+        }
+        public static string Day(int d)
+        {
+            return d switch
+            {
+                0 => "Sunday",
+                1 => "Monday",
+                2 => "Tuesday",
+                3 => "Wednesday",
+                4 => "Thursday",
+                5 => "Friday",
+                6 => "Saturday",
+                _ => "Incorrect"
+            };
+        }
+
+
+
+    }
+
+
+
+}
---
+++ CalenderFunction/CalenderFunction.cs
@@ -0,0 +1,12 @@
+﻿namespace CalenderLibrary1
+{
+    public class CalenderFunction
+    {
+        public static int dayOfDate(int d, int m, int y)
+        {
+            int[] t = { 0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4 };
+            y -= (m < 3) ? 1 : 0;
+            return (y + y / 4 - y / 100 + y / 400 + t[m - 1] + d) % 7;
+        }
+    }
+}
